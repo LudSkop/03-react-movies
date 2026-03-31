22a57eea84b9316cfe88fac58ed7610a78b2c761
LudSkop <luda.skopenko80@gmail.com>
2026-03-31 14:48:11 +0300

    create MovieModal

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,32 +5,55 @@ import type { Movie } from "../../types/movie";
 import { fetchMovies } from "../../services/movieService";
 import MovieGrid from "../MovieGrid/MovieGrid";
 import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import MovieModal from "../MovieModal/MovieModal";
 
 export default function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+
+  const handleOpenModal = (movie: Movie) => {
+    setSelectedMovie(movie);
+  };
 
   const handleSearchSubmit = async (query: string) => {
     setLoading(true); // показуємо лоадер
-    const data = await fetchMovies(query);
-    setLoading(false); // ховаємо лоадер після отримання даних
-    if (data.results.length === 0) {
-      toast.error("No movies found for your search query");
-      return;
+    setError(false); // скидаємо помилку перед новим запитом
+    try {
+      const data = await fetchMovies(query);
+      setLoading(false); // ховаємо лоадер після отримання даних
+      if (data.results.length === 0) {
+        toast.error("No movies found for your search query");
+        return;
+      }
+
+      setMovies(data.results);
+    } catch {
+      setLoading(false);
+      setError(true);
     }
-    setMovies(data.results);
 
     console.log("Search query in App:", query);
   };
+  console.log("selectedMovie:", selectedMovie); // ← додай цей рядок
   return (
     <>
       <Toaster position="top-center" reverseOrder={false} />
       <SearchBar onSubmit={handleSearchSubmit} />
-      <MovieGrid
-        onSelect={(movie) => console.log("Selected movie:", movie)}
-        movies={movies}
-      />
       {loading && <Loader />} {/* показуємо лоадер, якщо loading true */}
+      {error && <ErrorMessage />}
+      {/* показуємо повідомлення про помилку, якщо error true */}
+      {!loading && !error && (
+        <MovieGrid onSelect={handleOpenModal} movies={movies} />
+      )}
+      {selectedMovie && (
+        <MovieModal
+          movie={selectedMovie}
+          onClose={() => setSelectedMovie(null)}
+        />
+      )}
     </>
   );
 }
